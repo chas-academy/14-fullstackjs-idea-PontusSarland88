45299543e4316c36336ae956ba4b9d442a73388c
Pontus Särland <pontus.sarland@chasacademy.se>
2018-09-13 15:29:53 +0200

    basic setup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+    'mongodb://admin:password123@ds155352.mlab.com:55352/webshopdb'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const products = require('./routes/api/products');
+
+const app = express();
+
+// DB confix
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/products', products);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
